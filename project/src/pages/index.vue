84d19b037cd394e3d3b8d49534c82ba918f84e09
LXQ <515835543@qq.com>
2018-04-12 21:31:53 +0800

    fix
    Signed-off-by: LXQ <515835543@qq.com>

---
+++ project/src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="kolento">
+        <div class="banner">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .banner {background: url('../assets/images/banner.jpg') no-repeat;background-size: 100%;height: 70rem;}
+</style>
