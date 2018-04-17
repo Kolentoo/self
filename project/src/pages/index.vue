2bae29a1e430534bfdea3d14f31283e760d875d3
LXQ <515835543@qq.com>
2018-04-17 21:30:42 +0800

    fix
    Signed-off-by: LXQ <515835543@qq.com>

--- project/src/pages/index.vue
+++ project/src/pages/index.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="kolento">
         <div class="banner">
-
+            <div class="title">
+                <h1>让美好触手可及！</h1>
+                <h2>BY KOLENTO</h2>
+            </div>
         </div>
     </div>
 </template>
@@ -12,10 +15,19 @@
             return{
 
             }
+        },
+        created(){
+            
         }
     }
 </script>
 
 <style scoped>
-    .banner {background: url('../assets/images/banner.jpg') no-repeat;background-size: 100%;height: 70rem;}
+    .banner {background: url('../public/images/bj2.jpg') no-repeat center center;
+    background-size: 100%;color:#fff;display: flex;align-items: center;text-align: center;}
+    .title {width: 100%;height: 100%;}
+    h1{font-size: 5.4rem;letter-spacing: 1.4rem;width: 100%;}
+    h2{font-family: 'druk_pierre_testregular';font-size: 4.8rem;text-transform: uppercase;
+    letter-spacing: 1rem;font-weight: 100;width: 100%;}
+    /*.banner {background: url('../assets/images/banner.jpg') no-repeat;background-size: 100%;height: 70rem;}*/
 </style>
