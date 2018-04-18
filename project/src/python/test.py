a92ffece67ae682eec188eff7672dfc7e7cdeb51
LXQ <515835543@qq.com>
2018-04-18 21:43:55 +0800

    pic fixed
    Signed-off-by: LXQ <515835543@qq.com>

--- project/src/python/test.py
+++ project/src/python/test.py
@@ -14,8 +14,7 @@ page_info = page_info.decode('utf-8')
 # 将获取到的内容转换成BeautifulSoup格式，并将html.parser作为解析器
 soup = BeautifulSoup(page_info, 'html.parser') 
 # print(soup.prettify())
-titles = soup.find_all('div','more-link')
-
+titles = soup.find_all('a')
 for title in titles: 
     print(title.string) 
 # with open(r'F:\python\data.txt', 'w') as file:
@@ -23,6 +22,10 @@ for title in titles:
 #         file.write(title.string + '\n')
 
 #open()是读写文件的函数,with语句会自动close()已打开文件  
-with open(r"F:\python\data.txt","w") as file:       #在磁盘以只写的方式打开/创建一个名为 articles 的txt文件  
-    for title in titles:  
-        file.write(title.string+'\n')  
+
+#在磁盘以只写的方式打开/创建一个名为 articles 的txt文件  
+
+with open(r"F:\Python\data.txt","w") as file:       #在磁盘以只写的方式打开/创建一个名为 articles 的txt文件  
+    for title in titles:  
+        file.write(title.text+'\n')  
+        file.write("https://bangumi.bilibili.com/22/?spm_id_from=333.334.primary_menu.7" + title.get('href')+'\n\n') 
